427db318b402adcd661a15a2af2fcaff9ead121e
Tanida <printer.rung@gmail.com>
2024-12-20 15:41:40 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,24 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+using namespace std;
+
+int main(){
+
+    string Grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    int numGrade = sizeof(Grade)/sizeof(Grade[0]);
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+    
+    srand(time(0));
+    int realGrade = rand()% numGrade;
+    cout << "You will get " << Grade[realGrade] << " in this 261102.";
+
+
+    return 0;
+}
